c4bcaa4585379a43739f9ef52294f182db1253d5
GunnerUjjwol <gunner04ujjwol@gmail.com>
2017-02-22 15:47:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,8 +5,60 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articleone={
+    title: "Article One | Ujjwol Dandekhya",
+    heading: "Article One",
+    date: "Feb 22.2017",
+    content: `<div>
+            This is the content for my first article . And this is all the mess I could muster to fill this page up a little bit. I
+            I am only just trying .
+            
+            Lets see....
+        </div>`
+}
+
+function createTemplate(data){
+var title=data.title;
+var date=data.date;
+var heading=data.heading;
+var content=data.content;
+var htmlTemplate=`
+    <!DOCTYPE html>
+<html>
+    <head>
+        <title>
+        ${title}
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+       <link href="ui/style.css" rel="stylesheet" />
+        
+    </head>
+    <body>
+        <div class="container">
+        <div>
+            <a href="/">HOME</a>
+        </div>
+        <hr/>
+        <h3>
+            ${heading}
+        </h3>
+        <div>
+            ${date}
+        </div>
+       ${content}
+        </div>
+    </body>
+    
+    
+</html>
+`
+return htmlTemplate;
+}
+
+
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.send(createTemplate(articleone));
 });
 
 app.get('/article-one', function (req,res){
