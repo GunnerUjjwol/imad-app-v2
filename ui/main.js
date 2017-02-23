0ea1f9b1da757e83a80f8b315380f9b363f26be8
GunnerUjjwol <gunner04ujjwol@gmail.com>
2017-02-23 22:40:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,14 @@ console.log('Loaded!');
 //change the text of the main-text div
 var element= document.getElementById('main-text');
 element.innerHTML="new value";
+
+//move the image
+var img=document.getElementById('madi');
+var  marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft+10;
+    img.style.marginLeft= marginLeft + 'px';
+}
+img.onclick= function(){
+    var interval= setInterval(moveRight, 5);
+}
