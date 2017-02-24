1c7eaef6d21688839e4f27b89437e3c889977595
GunnerUjjwol <gunner04ujjwol@gmail.com>
2017-02-24 15:14:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,14 +28,30 @@ var nameInput=document.getElementById('nameInput');
 var name=nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick= function(){
-    //Make a request to the server and send the name
+    //Create a request object
+    var request= new XMLHttpRequest();
     
-    //Capture the list of names Render them as a list
-    var names=['name1','name2','name3','name4'];
-    var list='';
-    for( var i=0; i<names.length; i++){
-        list+='<li>'+ names[i] + '</li>';
-    }
-    var ul= document.getElementById('namelist');
-    ul.innerHTML= list;
-}
+    //capture response and store it in a variable
+    request.onreadystatechange= function(){
+        if (request.readyState== XMLHttpRequest.DONE){
+            //Take some action
+            if (request.status== 200){
+              //Capture the list of names, render as a list
+                var names=request.responseText;
+                names=JSON.parse(names);
+                var list='';
+                for( var i=0; i<names.length; i++){
+                    list+='<li>'+ names[i] + '</li>';
+                }
+                var ul= document.getElementById('namelist');
+                ul.innerHTML= list;
+            }
+        }
+        //Not done yet
+    };
+    
+    //Make the request
+    request.open('GET','http://gunnerujjwol.imad.hasura-app.io/submit-name>name=' +name, true);
+    request.send(null);
+
+};
