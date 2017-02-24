ad447d206a92b68cc240fc515dc48a21956e8a5a
GunnerUjjwol <gunner04ujjwol@gmail.com>
2017-02-24 13:25:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,8 +92,11 @@ app.get('/:articleName', function (req,res){
     res.send(createTemplate(articles[articleName]));
 });
 
-
-
+counter=0;
+app.get('/counter',  function(req,res){
+   counter+=1;
+   res.send(counter.toString());
+});
 
 
 app.get('/ui/style.css', function (req, res) {
