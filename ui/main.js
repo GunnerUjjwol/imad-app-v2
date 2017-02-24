fd58da2762901d3ecbd27a6c23dc22caf578d563
GunnerUjjwol <gunner04ujjwol@gmail.com>
2017-02-24 13:56:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,24 @@
 var button=document.getElementById('counter');
-var counter=0;
+
 button.onclick= function(){
-    //make a request to the counter endpoint
+    //Create a request object
+    var request= new XMLHttpRequest();
     
     //capture response and store it in a variable
+    request.onreadystatechange= function(){
+        if (request.readyState=== XMLHttpRequest.DONE){
+            //Take some action
+            if (request.status=== 200){
+                var counter= request.responseText;
+                var span=document.getElementById('span');
+                span.innerHTML= counter.toString();
+            }
+        }
+        //Not done yet
+    }
     
-    //render the variable in the correct span
-    counter=counter+1;
-    var span=document.getElementByid('span');
-    span.innerHTML= counter.toString();
-}
+    //Make the request
+    request.open('GET','http://gunnerujjwol.imad.hasura-app.io/counter', true);
+    request.send(null);
+    
+};
