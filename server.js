ba2efc3b6fa9c973ae04ce74192a56f1c8e4a9cb
GunnerUjjwol <gunner04ujjwol@gmail.com>
2017-02-24 17:59:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool=require('pg').Pool;
 
+var config={
+    user: 'gunnerujjwol',
+    database: 'gunnerujjwol',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+    
+};
 var app = express();
 app.use(morgan('combined'));
 
@@ -77,7 +86,19 @@ var htmlTemplate=`
 return htmlTemplate;
 }
 
-
+var pool = new Pool(config);
+app.get('/',function(){
+    //make a select request
+    //return response with a result
+    pool.query('SELECT * FROM test', function(err,result){
+        if (err){
+            res.status(500).send(err.toString());
+            }
+            else{
+                res.send(JSON.stringify(result));
+            }
+    });
+});
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
