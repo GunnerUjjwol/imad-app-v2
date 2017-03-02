bf02a59c8339a0565a393b3797a274d428f132d3
GunnerUjjwol <gunner04ujjwol@gmail.com>
2017-03-02 14:49:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool=require('pg').Pool;
+var crypto = require('crypto');
 
 var config={
     user: 'gunnerujjwol',
@@ -52,6 +53,23 @@ var htmlTemplate=`
 return htmlTemplate;
 }
 
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+function hash(input,salt){
+    //How do we create hash
+    var hashed= crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    return hashed.toString('hex');
+}
+
+app.get('/hash/:input',function (req,res){
+    var hashedString= has(req.params.input, 'this-is-some-random-string');
+    res.send(hashedString);
+});
+
+//establishing connection pool to the database
 var pool = new Pool(config);
 
 app.get('/test-db',function(req,res){
@@ -67,9 +85,6 @@ app.get('/test-db',function(req,res){
     });
 });
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
 
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
